b18f7503801a6c0841a7edb748ea330d98c67f0a
Michael Jung <MichaelWJung@users.noreply.github.com>
2020-01-29 22:44:13 +0100

    Clean up parts of the Application class

--- app/src/main/java/dev/michaeljung/clojurereframe/Application.java
+++ app/src/main/java/dev/michaeljung/clojurereframe/Application.java
@@ -8,13 +8,10 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.liquidplayer.javascript.JSON;
 import org.liquidplayer.service.MicroService;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,6 +22,7 @@ import java.util.ArrayList;
 public class Application extends android.app.Application {
 
     private static final String LOG_TAG = Application.class.getSimpleName();
+    private static final String DB_FILE_NAME = "db";
 
     private MicroService clojure;
     private boolean clojure_ready;
@@ -54,52 +52,19 @@ public class Application extends android.app.Application {
         final MicroService.EventListener readDbListener = new MicroService.EventListener() {
             @Override
             public void onEvent(MicroService service, String event, JSONObject payload) {
-                try {
-                    File file = new File(Application.this.getFilesDir(), "db");
-                    byte[] encoded = Files.readAllBytes(file.toPath());
-                    String content = new String(encoded, StandardCharsets.UTF_8);
-                    Log.v(LOG_TAG, "File db successfully loaded. Content: " + content);
-                    clojure.emit("initialize", content);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Log.v(LOG_TAG, "Could not load file. Initializing with empty set of todos");
-                    clojure.emit("initialize", "{}");
-                }
+                initializeDbFromFile();
             }
         };
-
-        final MicroService.EventListener readyListener = new MicroService.EventListener() {
-            @Override
-            public void onEvent(MicroService service, String event, JSONObject payload) {
-                synchronized (Application.this) {
-                    Application.this.clojure_ready = true;
-
-                    Log.v(LOG_TAG, "Running waiting runnables on main thread");
-                    for (Runnable r : waiting_ready_listeners) {
-                        new Handler(Looper.getMainLooper()).post(r);
-                    }
-                    waiting_ready_listeners.clear();
-
-                    Log.v(LOG_TAG, "Registering pending subscriptions");
-                    for (Subscription s : unregistered_subscriptions) {
-                        doSubscribe(s.id, s.query, s.listener);
-                    }
-                    unregistered_subscriptions.clear();
-                }
-            }
-        };
-
         final MicroService.EventListener storeListener = new MicroService.EventListener() {
             @Override
             public void onEvent(MicroService service, String event, JSONObject payload) {
-                try {
-                    String db = payload.getString("value");
-                    FileOutputStream fos = Application.this.openFileOutput("db", Context.MODE_PRIVATE);
-                    fos.write(db.getBytes(StandardCharsets.UTF_8));
-                    Log.v(LOG_TAG, "Saved todos to file. Content: " + db);
-                } catch (JSONException | IOException e) {
-                    e.printStackTrace();
-                }
+                saveDbToFile(payload);
+            }
+        };
+        final MicroService.EventListener readyListener = new MicroService.EventListener() {
+            @Override
+            public void onEvent(MicroService service, String event, JSONObject payload) {
+                carryOutReadyActions();
             }
         };
 
@@ -107,51 +72,81 @@ public class Application extends android.app.Application {
             @Override
             public void onStart(MicroService service) {
                 clojure.addEventListener("waiting-for-db", readDbListener);
-                clojure.addEventListener("ready", readyListener);
                 clojure.addEventListener("store", storeListener);
+                clojure.addEventListener("ready", readyListener);
             }
         };
 
-        URI uri = null;
-        try {
-            uri = new URI("android.resource://dev.michaeljung.clojurereframe/raw/app");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        clojure = new MicroService(this, uri, startListener);
-        Log.d(LOG_TAG, "Starting micro service");
-        clojure.start();
+        startClojureMicroService(startListener);
     }
 
-    void doWhenReady(final Runnable runnable) {
+    synchronized void doWhenReady(final Runnable runnable) {
         if (clojure_ready) {
             Log.v(LOG_TAG, "Running runnable right away");
             runnable.run();
-        } else synchronized (this) {
+        } else {
             Log.v(LOG_TAG, "Putting runnable on waiting ready listeners list");
             waiting_ready_listeners.add(runnable);
         }
-
     }
 
     void dispatch(JSONArray event) {
+        // TODO: clojure might not be ready
         clojure.emit("dispatch", event);
     }
 
-    void subscribe(String id, JSONArray query, EventListener listener) {
+    synchronized void subscribe(String id, JSONArray query, EventListener listener) {
         if (clojure_ready) {
             doSubscribe(id, query, listener);
-        } else synchronized (this) {
+        } else {
             Log.v(LOG_TAG, "Add subscription to list of pending subscriptions");
             unregistered_subscriptions.add(new Subscription(id, query, listener));
         }
     }
 
-    void unsubscribe(String id) {
-        // TODO: if not ready and still in unregistered_subscriptions: remove from there
+    synchronized void unsubscribe(String id) {
         if (clojure_ready) {
             clojure.emit("deregister", id);
+        } else {
+            // TODO: remove from unregistered_subscriptions
         }
+    }
+
+    private void startClojureMicroService(MicroService.ServiceStartListener startListener) {
+        URI uri = getCompiledClojureScriptCodeUri();
+        clojure = new MicroService(this, uri, startListener);
+        Log.d(LOG_TAG, "Starting micro service");
+        clojure.start();
+    }
+
+    private URI getCompiledClojureScriptCodeUri() {
+        try {
+            return new URI("android.resource://dev.michaeljung.clojurereframe/raw/app");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Error loading ClojureScript code");
+        }
+    }
+
+    private synchronized void carryOutReadyActions() {
+        Application.this.clojure_ready = true;
+        runWaitingReadyListeners();
+        registerPendingSubscriptions();
+    }
+
+    private void registerPendingSubscriptions() {
+        Log.v(LOG_TAG, "Registering pending subscriptions");
+        for (Subscription s : unregistered_subscriptions) {
+            doSubscribe(s.id, s.query, s.listener);
+        }
+        unregistered_subscriptions.clear();
+    }
+
+    private void runWaitingReadyListeners() {
+        Log.v(LOG_TAG, "Running waiting runnables on main thread");
+        for (Runnable r : waiting_ready_listeners) {
+            new Handler(Looper.getMainLooper()).post(r);
+        }
+        waiting_ready_listeners.clear();
     }
 
     private void doSubscribe(String id, JSONArray query, final EventListener listener) {
@@ -178,5 +173,43 @@ public class Application extends android.app.Application {
             e.printStackTrace();
         }
         clojure.emit("register", payload);
+    }
+
+    private void initializeDbFromFile() {
+        String content = readDbFile();
+        if (content == null) {
+            content = "{}";
+        }
+        clojure.emit("initialize", content);
+    }
+
+    private void saveDbToFile(JSONObject db) {
+        try {
+            writeDbFile(db.getString("value"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readDbFile() {
+        File file = new File(this.getFilesDir(), DB_FILE_NAME);
+        try {
+            byte[] encoded = Files.readAllBytes(file.toPath());
+            String content = new String(encoded, StandardCharsets.UTF_8);
+            Log.v(LOG_TAG, "File db successfully loaded. Content: " + content);
+            return content;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private void writeDbFile(String content) {
+        try {
+            FileOutputStream fos = Application.this.openFileOutput(DB_FILE_NAME, Context.MODE_PRIVATE);
+            fos.write(content.getBytes(StandardCharsets.UTF_8));
+            Log.v(LOG_TAG, "Saved todos to file. Content: " + content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
